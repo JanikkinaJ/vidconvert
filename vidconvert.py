a1db853f887e614d33c413c1bd81ca6301307e38
kineye <kineye@localhost.localdomain>
2022-05-18 20:24:27 +0200

    added rich bar and optimized code

--- vidconvert.py
+++ vidconvert.py
@@ -1,68 +1,70 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-from rich.progress import Progress #to replace tqdm
-from tqdm import tqdm
-from time import sleep
-import os
-import subprocess
+from rich.progress import (
+        BarColumn,
+        Progress,
+        TextColumn,
+        )
+from time import sleep; import os; import subprocess
+
+workDir = os.curdir # the current working Directory TODO: add ability for user to choose this
+supportedList = ['.mkv', '.avi']
 
 # move the files into directories named after their types
-def sort_file(new_filename, filename, old_type):
-    path = os.curdir
-    path = path + '/'
+def sort_file(newFilename, currentFile, oldType):
+    path = workDir + '/'
     try:
-        os.rename(path + filename, path + old_type + "/" + filename)
-        os.rename(path + new_filename, path + "mp4/" + new_filename)
+        os.rename(path + currentFile, path + oldType + "/" + currentFile)
+        os.rename(path + newFilename, path + "mp4/" + newFilename)
+        return "Moved"
     except OSError as error:
-            tqdm.write(str(error))
+        return str(error)
 
 # converts a file into mp4 using ffmpeg
-def convert(old_type, filename):
-    new_filename = filename.replace(old_type, "mp4")
-    subprocess.run(['ffmpeg', '-hide_banner', '-loglevel', 'error','-i', filename, '-codec', 'copy', new_filename])
-    tqdm.write(str(filename) + " was converted to " + str(new_filename))
-    sort_file(new_filename, filename, old_type)
+def convert(oldType, currentFile):
+    newFilename = currentFile.replace(oldType, "mp4")
+    subprocess.run(['ffmpeg', '-hide_banner', '-loglevel', 'error','-i', currentFile, '-codec', 'copy', newFilename])
+    sort_file(newFilename, currentFile, oldType)
+    return f"\"{currentFile}\" was converted to \"{newFilename}\""
 
-# uses convert() and then adds a folder for the type if not already created
-def process(folder_present, mp4_folder, filename, endswith):
-    if not folder_present: 
-        add_folder(endswith)
-    if not mp4_folder:
-        add_folder("mp4")
-    convert(endswith, filename)
+def add_folder(vidType): #creates a folder
+    try:
+        os.mkdir(vidType)
+        print("Directory " + str(vidType) + " created")
+    except FileExistsError:
+        #print("Directory " + str(vidType) + " already exists")
+        return None
 
-#checks current directory for supported file types and creates a mp4 version of them
 def vidconvert():
-    mkv_folder = False; avi_folder = False; mp4_folder  = False
-    for filename in tqdm(os.listdir(os.curdir), desc ="Progress", unit =" file", bar_format= '{desc}: {percentage:3.0f}%|{bar}|{n_fmt}/{total_fmt}'): 
-        sleep(0.1)
-        if filename.endswith('.mkv'):
-            process(mkv_folder, mp4_folder, filename, "mkv")
-            mkv_folder = True
-            mp4_folder = True
-        elif filename.endswith('.avi'):
-            process(avi_folder, mp4_folder, filename, "avi")
-            avi_folder = True
-            mp4_folder = True
-        else:
-            tqdm.write(str(filename) + " ignored as type isn't supported")
-            continue
+    with Progress(
+            TextColumn("{task.description}", justify="right"),
+            BarColumn(bar_width=None),
+            "[progress.percentage]{task.percentage:>3.1f}%"
+            )  as progress:
+        fileNumber = len([name for name in os.listdir(workDir) if os.path.isfile(name)])
+        task = progress.add_task("Progress: ", total=fileNumber)
+        for currentFile in os.listdir(workDir):
+            extension = os.path.splitext(currentFile)[1]
+            if extension in supportedList:
+                progress.console.print(f"Converting: \"{currentFile}\"")
+                add_folder("mp4")
+                add_folder(extension[1:])
+                progress.console.print(convert(extension[1:], currentFile))
+                progress.advance(task)
+            else:
+                progress.console.print(f"\"{currentFile}\" ignored as type isn't supported")
+                sleep(0.2)
+                progress.advance(task)
+
     print("""
-         _       _                                                _   
-        (_)     | |                                              | |  
-__   __  _    __| |   ___    ___    _ __   __   __   ___   _ __  | |_ 
-\ \ / / | |  / _` |  / __|  / _ \  | '_ \  \ \ / /  / _ \ | '__| | __|
- \ V /  | | | (_| | | (__  | (_) | | | | |  \ V /  |  __/ | |    | |_ 
-  \_/   |_|  \__,_|  \___|  \___/  |_| |_|   \_/    \___| |_|     \__|
+          _       _                                                _   
+         (_)     | |                                              | |  
+ __   __  _    __| |   ___    ___    _ __   __   __   ___   _ __  | |_ 
+ \ \ / / | |  / _` |  / __|  / _ \  | '_ \  \ \ / /  / _ \ | '__| | __|
+  \ V /  | | | (_| | | (__  | (_) | | | | |  \ V /  |  __/ | |    | |_ 
+   \_/   |_|  \__,_|  \___|  \___/  |_| |_|   \_/    \___| |_|     \__|
 """)
 
-
-def add_folder(vid_type): #creates folder
-    try:
-        os.mkdir(vid_type)
-        tqdm.write("Directory " + str(vid_type) + " created")
-    except FileExistsError:
-        tqdm.write("Directory" + str(vid_type) + " already exists")
 
 if __name__ == "__main__":
     vidconvert()
