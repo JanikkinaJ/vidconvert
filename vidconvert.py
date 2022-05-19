ce2235c9de61ee6ae9463e63413954e5597876cc
kineye <kineye@localhost.localdomain>
2022-05-19 21:29:10 +0200

    issue #8

--- vidconvert.py
+++ vidconvert.py
@@ -8,27 +8,32 @@ from rich.progress import (
 from rich.console import Console
 from time import sleep; import os; import subprocess
 import argparse
+
+supportedList = ['.mkv', '.avi'] #supported video types
+convertTo = ".mp4"
+workDir = os.curdir # the current working Directory TODO: add ability for user to choose this
+
 console = Console() #creates a console type for Rich text output
 parser = argparse.ArgumentParser(description= 'Mass batch conversion using ffmpeg. Converts video files into Mp4 and then sorts them.') 
 parser.add_argument("-v","--verbose", help="increase output verbosity", action="store_true")
+parser.add_argument("-ot","--outputtype", help="lets the user choose an outputtype via extension", type=str, choices=supportedList)
 args = parser.parse_args() #argparse used to allow command line arguments
-
-workDir = os.curdir # the current working Directory TODO: add ability for user to choose this
-supportedList = ['.mkv', '.avi'] #supported video types
+if args.outputtype is not None:
+    convertTo = str(args.outputtype)
 
 # move the files into directories named after their types
 def sort_file(newFilename, currentFile, oldType):
     path = workDir + '/'
     try:
         os.rename(path + currentFile, path + oldType + "/" + currentFile)
-        os.rename(path + newFilename, path + "mp4/" + newFilename)
+        os.rename(path + newFilename, path + convertTo[1:] +"/" + newFilename)
         return f"\"{currentFile}\" was converted to \"{newFilename}\""
     except OSError as error:
         return str(error)
 
 # converts a file into mp4 using ffmpeg
 def convert(oldType, currentFile):
-    newFilename = currentFile.replace(oldType, "mp4")
+    newFilename = currentFile.replace(oldType, convertTo[1:])
     result = subprocess.run(['ffmpeg', '-hide_banner', '-loglevel', 'error','-i', currentFile, '-codec', 'copy', newFilename], text=True, capture_output=True)
     output = result.stderr + "\n" + sort_file(newFilename, currentFile, oldType)
     return output 
@@ -52,10 +57,10 @@ def vidconvert():
         ignored = 0
         for currentFile in os.listdir(workDir):
             extension = os.path.splitext(currentFile)[1]
-            if extension in supportedList:
+            if extension in supportedList && extension != convertTo:
                 if args.verbose:
                     progress.console.print(f"Converting: \"{currentFile}\"")
-                add_folder("mp4")
+                add_folder(convertTo[1:])
                 add_folder(extension[1:])
                 progress.console.print(convert(extension[1:], currentFile))
                 progress.advance(task)
